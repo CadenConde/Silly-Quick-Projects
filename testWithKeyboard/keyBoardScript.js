88d993f853b5bc040ecce6504c43b6660a629c09
Caden Conde <61475148+CadenConde@users.noreply.github.com>
2024-10-03 02:40:50 -0400

    Delete testWithKeyboard directory

--- testWithKeyboard/keyBoardScript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function (event) {
-    var key = document.getElementById(event.code);
-    key.className = "active";
-});
-document.addEventListener('keyup', function(event) {
-    var key = document.getElementById(event.code);
-    key.className = "";
-});
-  
